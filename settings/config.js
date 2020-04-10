ea00094d4ab1ecfed31661e8eaf63d8b0d5626d3
Big-Boss69 <63454196+Big-Boss69@users.noreply.github.com>
2020-04-10 14:41:26 +0300

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	codename: "Big Boss Bot| Бот Большого бота",//Кодовое имя
+	namebot: "Big Boss Bot| Бот Большого бота", //Имя бота
+	version: "3.1", //Версия кода
+	admin: "[id62275824|Big Boss]", //2 руководитель бота
+	coder: "[id62275824|Big Boss]", //Для доната
+	zam: "[id473375278|Small Boss]", //Заместитель
+	site: "В группе", //Сайт, если есть
+	group_url: "[Big Boss Bot| Бот Большого бота]",//12213
+}
